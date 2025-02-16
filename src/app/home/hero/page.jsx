d00e3c3d0c05988eff0896bca787e5031880bfb5
terejoe <nzeamalunkechinyere@gmail.com>
2025-02-16 19:46:07 +0100

    Hero items taking up space

--- src/app/home/hero/page.jsx
+++ src/app/home/hero/page.jsx
@@ -5,17 +5,17 @@ import Link from "next/link";
 const Hero = () => {
   return (
     <div
-      className="bg-center bg-cover h-screen md:h-[calc(100vh-74px)] w-full flex justify-center items-center"
+      className="bg-center bg-cover md:h-[calc(100vh-74px)] w-full flex justify-center items-center"
       style={{
         backgroundImage: `url('/hero_bg.png')`, // Correct way to set the URL as a string
       }}
     >
-      <div className="text-white text-left md:flex gap-8 p-8">
-        <div className="max-w-[580px] tracking-wider">
-          <h1 className="text-4xl font-bold leading-[1.5]">
+      <div className="text-white text-left md:flex gap-8 p-4">
+        <div className="max-w-[740px] md:pt-20 pt-10">
+          <h1 className="text-4xl md:text-5xl font-bold leading-[1.5] tracking-widest">
             Together, Let's Create a World Where Everyone Matters.
           </h1>
-          <p className="mt-4 text-sm font-light leading-[1.8]">
+          <p className="mt-4 text-sm font-light leading-[1.8] max-w-[550px]">
             Join us in our mission to create a positive impact, supporting
             communities in need, one step at a time.
           </p>
@@ -32,7 +32,7 @@ const Hero = () => {
           <Image
             src={HeroImg}
             className="rounded-2xl"
-            width={350}
+            // width={350}
             alt="Hero Image of people standing and smiling"
           />
         </div>
