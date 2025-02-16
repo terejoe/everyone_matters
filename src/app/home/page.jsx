72577cf393675acb45616add566bbffde9e177f3
terejoe <nzeamalunkechinyere@gmail.com>
2025-02-16 19:54:20 +0100

    Trying to resolve padding isssue

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -11,7 +11,7 @@ const Home = () => {
     <div>
       <Hero/>
       <Creating/>
-      {/* <Reports/> */}
+      <Reports/>
       <CoreFocus/>
       <Involved/>
       <Footer/>
--- src/app/page.js
+++ src/app/page.js
@@ -11,7 +11,7 @@ export default function Home() {
     <div className="">
       <Hero/>
       <Creating/>
-      {/* <Reports/> */}
+      <Reports/>
       <CoreFocus/>
       <Involved/>
       <Footer/>
